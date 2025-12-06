69b67351fa6a34440f304d3453298361446b4864
AvinashAbbigeri <avinashabbigeri40@gmail.com>
2025-12-06 21:04:54 +0530

    Initial ClearDSA deploy
    Signed-off-by: AvinashAbbigeri <avinashabbigeri40@gmail.com>

---
+++ cleardsa/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div id='hero'>
+      <div id="content">
+        <h1 className="title">ClearDSA</h1>
+        <h3 className="description">DSA made simple, understand the "Why?".</h3>
+      </div>
+    </div>
+  );
+}
